f0ebd4986607ffd64bee2171f1e38c180360809e
cosin <rescp17@gmail.com>
2023-08-20 10:43:28 +0800

    feat: auth guard

--- apps/realworld-server/src/app/app.controller.ts
+++ apps/realworld-server/src/app/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard'
 
 @Controller()
 export class AppController {
@@ -9,5 +10,19 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  /**
+   * {
+   *   "username": "xxxx",
+   *   "password": "xxxx"
+   * }
+   * it must be above
+   */
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    console.log(req)
+    return req.user;
   }
 }
--- apps/realworld-server/src/app/auth/auth.service.ts
+++ apps/realworld-server/src/app/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {  Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  constructor(private userService: UserService) {}
+
+  async vaildateUser(email: string, password: string) {
+    const existingUserByEmail = await this.userService.findByEmail(email)
+    if (!existingUserByEmail || existingUserByEmail.password !== password) {
+      throw new UnauthorizedException()
+    }
+    return existingUserByEmail
+  }
+}
